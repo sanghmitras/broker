238177ce17d1e33d39419e90093c1af439772ea9
Sanghmitra Singh Rathore <sanghmitra1993@gmail.com>
2023-04-14 09:14:27 +0530

    reading from json file

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,24 +1,117 @@
-function fetchdata(){
-    fetch("http://localhost:3000/api/test-suit").then(resp=>resp.json()).then(resp=>{
-        // console.log("resp", resp);
-        let select_type = document.getElementById("select_type")
-        let inhtml = ''
-        resp.map(i=>{
-            inhtml = inhtml + `<option value="${i.name}">${i.name}</option>`
-        })
-        select_type.innerHTML = inhtml
-    }).catch(err=>{
-        console.log('err', err);
+function fetchdata() {
+  fetch("http://localhost:3000/api/test-suit")
+    .then((resp) => resp.json())
+    .then((resp) => {
+      // console.log("resp", resp);
+      let select_type = document.getElementById("select_type");
+      let inhtml = "";
+      resp.map((i) => {
+        inhtml = inhtml + `<option value="${i}">${i}</option>`;
+      });
+      select_type.innerHTML = inhtml;
     })
+    .catch((err) => {
+      console.log("err", err);
+    });
+}
+function fetchSuit() {
+  fetch("http://localhost:3000/api/test-hierarchy")
+    .then((resp) => resp.json())
+    .then((resp) => {
+      //   console.log("resp", resp);
+      let select_type = document.getElementById("select_type");
+      let inhtml = "";
+      Object.keys(resp).map((item) => {
+        inhtml = inhtml + `<option value="${item}">${item}</option>`;
+        // console.log("item", item);
+      });
+      select_type.innerHTML = inhtml;
+    })
+    .catch((err) => {
+      console.log("err", err);
+    });
+}
+function fetchTest() {
+  fetch("http://localhost:3000/api/test-hierarchy")
+    .then((resp) => resp.json())
+    .then((resp) => {
+      //   console.log("resp", resp);
+      let select_type = document.getElementById("select_type");
+      let inhtml = "";
+      let allTest = [];
+      Object.keys(resp).map((item) => {
+        let tests = Object.keys(resp[item]);
+        // console.log("tst", tests);
+        tests.map((i) => {
+          allTest.push(i);
+        });
+      });
+      //   console.log("test case", allTest);
+      allTest.map((test) => {
+        inhtml = inhtml + `<option value="${test}">${test}</option>`;
+      });
+
+      select_type.innerHTML = inhtml;
+    })
+    .catch((err) => {
+      console.log("err", err);
+    });
+}
+function fetchId() {
+  fetch("http://localhost:3000/api/test-hierarchy")
+    .then((resp) => resp.json())
+    .then((resp) => {
+      //   console.log("resp", resp);
+      let select_type = document.getElementById("select_type");
+      let inhtml = "";
+      let allTest = [];
+      Object.keys(resp).map((item) => {
+        let tests = Object.keys(resp[item]);
+        // console.log("tst", tests);
+        tests.map((i) => {
+          //   console.log("i", i);
+          let values_with_id = Object.values(resp[item][i]);
+          values_with_id.map((v) => {
+            // console.log("values", v, item, i);
+            allTest.push(v);
+          });
+          // allTest.push(i);
+        });
+      });
+      //   console.log("test case", allTest);
+      allTest.map((test) => {
+        inhtml = inhtml + `<option value="${test}">${test}</option>`;
+      });
+
+      select_type.innerHTML = inhtml;
+    })
+    .catch((err) => {
+      console.log("err", err);
+    });
 }
 
-function handleTypeChange(){
-    fetchdata()
+function handleTypeChange() {
+  let seleted_by = document.getElementById("select_by");
+  switch (seleted_by.value) {
+    case "suit": {
+      fetchdata();
+      break;
+    }
+    case "test_case": {
+      fetchTest();
+      break;
+    }
+    case "id": {
+      fetchId();
+      break;
+    }
+  }
+  //   fetchHierarchy();
 }
 
-function main(){
-    // DOM loaded
-    // fetchdata();
-
+function main() {
+  // DOM loaded
+  //   fetchdata();
+  //   fetchHierarchy();
 }
-document.addEventListener('DOMContentLoaded', main());
+document.addEventListener("DOMContentLoaded", main());
--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,42 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const modal = require('../modal/modal');
- 
+const fs = require("fs");
+// const modal = require('../modal/modal');
 
 /* GET users listing. */
-router.get('/test-suit', function(req, res, next) {
-  modal.GetTestSuit().then(resp=>{
-    // console.log('result', resp)
-    res.send(JSON.stringify(resp))
-    res.status(200)
-    res.end()
-  }).catch(err=>{
-    console.log('error', err)
-    res.send({data: err})
-    res.status(500)
-    res.end()
-  })
+router.get("/test-suit", function (req, res, next) {
+  fs.readFile("../broker/public/test_suit.json", "utf8", function (err, data) {
+    if (err) throw err;
 
+    var resultArray = data;
+    // console.log("result", data);
+    res.send(resultArray);
+  });
+  // modal.GetTestSuit().then(resp=>{
+  //   // console.log('result', resp)
+  //   res.send(JSON.stringify(resp))
+  //   res.status(200)
+  //   res.end()
+  // }).catch(err=>{
+  //   console.log('error', err)
+  //   res.send({data: err})
+  //   res.status(500)
+  //   res.end()
+  // })
+});
+
+router.get("/test-hierarchy", function (req, res, next) {
+  fs.readFile(
+    "../broker/public/dir_hierarchy.json",
+    "utf8",
+    function (err, data) {
+      if (err) throw err;
+
+      var resultArray = data;
+      // console.log("result", data);
+      res.send(resultArray);
+    }
+  );
 });
 
 module.exports = router;
